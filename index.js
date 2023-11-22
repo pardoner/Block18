492c347fe4773dad04898a6d73dcee0428d93cb3
Dylan Wicker <dylanwicker@Dylans-Air.attlocal.net>
2023-11-22 13:22:49 -0500

    added instructions

---
+++ index.js
@@ -0,0 +1,32 @@
+function multiplication(x, y) {
+	return x * y;
+}
+
+// expect multiplication(5, 3) to be a number
+// expect multiplication(5, 3) to be equal to 15
+// expect multiplication("x", "y") to be an error
+// expect multiplication("x", 3) to be an error
+//expect multiplication(5, "y") to be an error
+
+function concatOdds(array1, array2) {
+
+}
+// expect concatOdds([5, 4, 1, 7], [-2, 11, 6, 2, -1, 3]) to return [-1, 1, 3, 5, 7, 11]
+// expect concatOdds([5, 4, 1, 7], "array2") to return an error 
+// expect concatOdds("array1", [-2, 11, 6, 2, -1, 3]) to return an error 
+// expect concatOdds(string, string) to return an error
+// expect concatOdds([2, 4, 6], [0, 10]) to be []
+
+
+
+function shoppingCart(array1, array2) {
+
+}
+
+// no items added in the cart should display the message "your cart is empty"
+// items added to your cart should become visible in the cart 
+// adding items to your cart should update and share the total price at the bottom
+// adding items to your cart should update and share the total quantity at the bottom
+// expected to prompt user to either create an account or check out as quest
+// expected to return an error "please enter a valid email address" if no email is entered
+
